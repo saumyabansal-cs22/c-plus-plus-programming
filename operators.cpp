f781b55cbe91ecccdd62fe56d8cde9b82720bdd2
Saumya Bansal <122472320+saumyabansal-cs22@users.noreply.github.com>
2023-02-12 20:32:54 +0530

    Add files via upload

---
+++ operators.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=9,b=8;
+    //cout<<a+b<<endl<<a-b<<endl<<a*b<<endl<<a/b<<endl;*/
+    cout<<((a==b)&&(a>b));
+    return 0;
+    int a = (float)a;
+    cout<<a;
+}
