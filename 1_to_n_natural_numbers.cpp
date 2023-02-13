e095e2b67029225d73289abea4c89e43edb9af42
Saumya Bansal <122472320+saumyabansal-cs22@users.noreply.github.com>
2023-02-13 23:34:08 +0530

    Add files via upload

---
+++ 1_to_n_natural_numbers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i,n;
+    cout<<"enter the number upto which you want to print natural numbers ;";
+    cin>>n;
+    for (i=1;i<=n;i++){
+        cout<<i<<" ";    }
+    return 0;
+}
